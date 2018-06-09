08252807393f5e669bf9749eec31b3c761df93cb
Harry Liu <byliuyang11@gmail.com>
2018-06-09 01:26:47 -0700

    Clean up test

--- config_test.go
+++ config_test.go
@@ -3,12 +3,10 @@ package main
 import (
 	"testing"
 	"github.com/stretchr/testify/assert"
-	"fmt"
 )
 
 func Test_loadConfig(t *testing.T) {
 	testConfigs, err := loadConfig("testdata/config_test.json")
 	assert.Nil(t, err)
 	assert.IsType(t, configs{}, testConfigs)
-	fmt.Println("configs", testConfigs)
 }
